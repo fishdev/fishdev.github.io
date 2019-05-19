b5d3898553ad52544dae62f2e3a9eb4feb595192
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-05-19 16:19:42 -0400

    Added transitions and shortcuts modal

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,27 +1,33 @@
-// Keybindings for social links and resume sections
-Mousetrap.bind('g', function () {
-  location.href = "https://github.com/fishdev";
+// Keybindings for links and resume sections
+Mousetrap.bind(['g', 'G'], function () {
+  location.href = 'https://github.com/fishdev';
 });
-Mousetrap.bind('l', function () {
-  location.href = "https://www.linkedin.com/in/ashwin-srinivasan-65188517a/";
+Mousetrap.bind(['l', 'L'], function () {
+  location.href = 'https://www.linkedin.com/in/ashwin-srinivasan-65188517a/';
 });
-Mousetrap.bind('m', function () {
-  location.href = "mailto:ashwins@andrew.cmu.edu";
+Mousetrap.bind(['m', 'M'], function () {
+  location.href = 'mailto:ashwins@andrew.cmu.edu';
 });
-Mousetrap.bind('e', function () {
+Mousetrap.bind(['r', 'R'], function () {
+  location.href = "resume.pdf";
+});
+Mousetrap.bind(['e', 'E'], function () {
   jump('education');
 });
-Mousetrap.bind('s', function () {
+Mousetrap.bind(['s', 'S'], function () {
   jump('skills');
 });
-Mousetrap.bind('p', function () {
+Mousetrap.bind(['p', 'P'], function () {
   jump('projects');
 });
-Mousetrap.bind('x', function () {
+Mousetrap.bind(['x', 'X'], function () {
   jump('experience');
 });
-Mousetrap.bind('a', function () {
+Mousetrap.bind(['a', 'A'], function () {
   jump('activities');
+});
+Mousetrap.bind(['/', '?'], function () {
+  shortcuts();
 });
 
 // Jump to anchor
@@ -29,4 +35,9 @@ function jump(h) {
   var url = location.href;
   location.href = '#' + h;
   history.replaceState(null, null, url);
+}
+
+// Toggle shortcuts modal
+function shortcuts() {
+  document.getElementById('shortcuts').classList.toggle('is-active');
 }
