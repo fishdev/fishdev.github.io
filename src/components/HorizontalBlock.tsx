f53efd389f627f3b89afd5cff9c1bd47a1bfceeb
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-07-25 15:32:05 -0400

    Awards in horizontal block

--- src/components/HorizontalBlock.tsx
+++ src/components/HorizontalBlock.tsx
@@ -4,9 +4,18 @@ import { Link } from 'react-router-dom';
 import { Block } from '../interfaces';
 import { BlockHeader } from './BlockHeader';
 
-export const HorizontalBlock: React.FC<Block> = props => (
-  <Link to={'/' + props.id} className="block box">
-    <BlockHeader {...props} />
-    <div className="content">{props.description}</div>
+export const HorizontalBlock: React.FC<Block> = block => (
+  <Link to={'/' + block.id} className="block box">
+    <BlockHeader {...block} />
+    <div className="content">{block.description}</div>
+    {block.awards &&
+      block.awards.map((award, i) => (
+        <p className="award" key={i}>
+          <span className="icon has-text-warning">
+            <i className="fas fa-award" />
+          </span>
+          <span>{award}</span>
+        </p>
+      ))}
   </Link>
 );
