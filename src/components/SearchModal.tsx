65b85b079601dc1383da6d6770b62a5e10eaee29
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-07-29 18:18:17 -0400

    Fixed search results scrolling

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -28,7 +28,7 @@ export class SearchModal extends React.PureComponent<Props> {
   handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     this.setState({ search: event.target.value });
 
-  searchResults = (): React.ReactNode => {
+  searchResults = (): React.ReactNode[] => {
     const results = filterEntities(this.state.search).map((entity: Entity) => {
       switch (entity.type) {
         case 'coursework':
@@ -41,46 +41,54 @@ export class SearchModal extends React.PureComponent<Props> {
           return <BlockBox {...skillToBlock(entity.data as Skill)} />;
       }
     });
-    return results.length > 0 ? (
-      results
-    ) : (
-      <div className="has-text-centered">
-        <span className="tag is-medium is-danger">
-          <span>Sorry, no results</span>
-          <span className="icon">
-            <i className="far fa-frown" />
-          </span>
-        </span>
-      </div>
-    );
+    return results;
   };
 
   render() {
     const { hide } = this.props;
     const { search } = this.state;
+    const results = this.searchResults();
     return (
       <HotKeys handlers={{ HIDE_SEARCH: hide }}>
         <div className="modal is-active">
           <div className="modal-background" />
-          <div className="modal-content">
-            <div className="field">
-              <div className="control has-icons-left">
-                <input
-                  ref={input => (this.searchInput = input)}
-                  className="input is-large"
-                  type="text"
-                  value={search}
-                  onChange={this.handleSearchChange}
-                  placeholder="Type to search..."
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-search" />
-                </span>
+          <div className="search-container modal-content">
+            <div className="search-box columns is-mobile is-vcentered is-variable is-1">
+              <div className="column">
+                <div className="field">
+                  <div className="control has-icons-left">
+                    <input
+                      ref={input => (this.searchInput = input)}
+                      className="input is-large"
+                      type="text"
+                      value={search}
+                      onChange={this.handleSearchChange}
+                      placeholder="Type to search..."
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-search" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-narrow">
+                <button className="delete is-large" onClick={hide} />
               </div>
             </div>
-            <div className="search-results">{search.length >= 2 && this.searchResults()}</div>
+            {search.length >= 2 &&
+              (results.length > 0 ? (
+                results
+              ) : (
+                <div className="has-text-centered">
+                  <span className="tag is-medium is-danger">
+                    <span>Sorry, no results</span>
+                    <span className="icon">
+                      <i className="far fa-frown" />
+                    </span>
+                  </span>
+                </div>
+              ))}
           </div>
-          <button className="modal-close is-large" onClick={hide} />
         </div>
       </HotKeys>
     );
