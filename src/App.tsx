8e0f86cc539ad15cf289d0f4d05612fe2d60724d
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-01 12:26:32 -0400

    Proper routing (no hash)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home, Resume, Details, Spotify } from './pages';
 
