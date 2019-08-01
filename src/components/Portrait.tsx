56a850a7862d1282adf6f867bc01a38edcff50a0
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-07-31 20:19:56 -0400

    Better favicon

--- src/components/Portrait.tsx
+++ src/components/Portrait.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ashwin from '../assets/ashwin.jpg';
 
 interface Props {
   size: string;
@@ -13,12 +14,7 @@ export class Portrait extends React.PureComponent<Props> {
     const { size } = this.props;
     return (
       <figure className={'image is-' + size}>
-        <img
-          className="is-rounded"
-          src={process.env.PUBLIC_URL + '/ashwin.jpg'}
-          id="profile"
-          alt="Me"
-        />
+        <img className="is-rounded" src={ashwin} id="profile" alt="Me" />
       </figure>
     );
   }
