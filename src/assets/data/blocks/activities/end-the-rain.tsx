898e1d300e996d92ec3034c748baeaad0ac6f92a
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-01 22:14:46 -0400

    Resume update
    Details for ETR

--- src/assets/data/blocks/activities/end-the-rain.tsx
+++ src/assets/data/blocks/activities/end-the-rain.tsx
@@ -26,4 +26,20 @@ export const end_the_rain: Block = {
       umbrella dispenser network, as well as Node.js + MongoDB backend system.
     </p>
   ),
+  info: (
+    <div>
+      <p>
+        I was one of the first members of End the Rain, an organization dedicated to mitigating
+        effects of weather at Carnegie Mellon. The Plaid Umbrella Project will install a system of
+        free umbrella dispensers around campus.
+      </p>
+      <p>
+        As the technical and software lead, I created and maintain the organization's website. In
+        addition, I am developing a Node.js + MongoDB backend system for the umbrella dispenser
+        network, as well as a mobile application to improve accessibility. I coordinate electrial
+        and mechanical engineering teams to perform integration tests on the umbrella dispenser
+        prototypes.
+      </p>
+    </div>
+  ),
 };
