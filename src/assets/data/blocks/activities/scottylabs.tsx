a7617758eef5b95ec4025ca4a3787aca85f1776f
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-02 14:26:34 -0400

    Resume update
    Details for ScottyLabs

--- src/assets/data/blocks/activities/scottylabs.tsx
+++ src/assets/data/blocks/activities/scottylabs.tsx
@@ -14,9 +14,14 @@ export const scottylabs: Block = {
   },
   buttons: [
     {
-      url: 'https://scottylabs.org/wdw',
+      url: 'https://scottylabs.org/',
       icon: 'fas fa-dog',
-      name: 'ScottyLabs Web Dev Weekend',
+      name: 'ScottyLabs',
+    },
+    {
+      url: 'https://scottylabs.org/wdw/react/sunday/slides.pdf',
+      icon: 'fas fa-square',
+      name: 'Web Dev Weekend Presentation',
     },
   ],
   description: (
@@ -24,5 +29,23 @@ export const scottylabs: Block = {
       Gave introductory React talk at <a href="https://scottylabs.org/wdw/">Web Dev Weekend</a>{' '}
       2018. Overhauled Python + Flask API to make printing accessible on-campus.
     </p>
+  ),
+  info: (
+    <div>
+      <p>
+        I am a member of ScottyLabs, an organization at Carnegie Mellon devoted to helping students
+        learn about technology.
+      </p>
+      <p>
+        Using Python and <a href="https://flask.palletsprojects.com/en/1.1.x/">Flask</a>, I
+        overhauled an API that allows students to easily create print jobs from a web browser or
+        mobile device.
+      </p>
+      <p>
+        At <a href="https://scottylabs.org/wdw">Web Dev Weekend 2018</a>, I gave a talk on
+        application development using React. This included a brief lecture and a hands-on activity
+        where I guided several students through their first React project.
+      </p>
+    </div>
   ),
 };
