f0f92571c668f4d29ac0fcea791d8262ea8b5178
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-02 14:37:46 -0400

    Fixed key for skill tags

--- src/components/SkillGroup.tsx
+++ src/components/SkillGroup.tsx
@@ -6,8 +6,8 @@ import { SkillTag } from './SkillTag';
 export const SkillGroup: React.FC<{ skills: Skill[] }> = ({ skills }) => (
   <div className="field is-grouped is-grouped-multiline skill-group">
     {skills.map(skill => (
-      <div className="control">
-        <SkillTag key={skill.name} {...skill} />
+      <div key={skill.name} className="control">
+        <SkillTag {...skill} />
       </div>
     ))}
   </div>
