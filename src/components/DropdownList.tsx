452f44b5bf3e9bd72aa3232ed6bac2d0c6d0a02b
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-02 12:28:57 -0400

    Dropup option, click away from dropdowns

--- src/components/DropdownList.tsx
+++ src/components/DropdownList.tsx
@@ -9,6 +9,7 @@ interface Props {
   items?: Dropdown[];
   hoverable?: boolean;
   alignment?: 'is-left' | 'is-right';
+  dropup?: boolean;
 }
 
 interface State {
@@ -16,6 +17,8 @@ interface State {
 }
 
 export class DropdownList extends React.PureComponent<Props, State> {
+  node: React.RefObject<HTMLDivElement> = React.createRef();
+
   state: Readonly<State> = {
     isActive: false,
   };
@@ -23,29 +26,55 @@ export class DropdownList extends React.PureComponent<Props, State> {
   static defaultProps: Partial<Props> = {
     hoverable: false,
     alignment: 'is-right',
+    dropup: false,
   };
 
-  toggleActive = () => this.setState(({ isActive }) => ({ isActive: !isActive }));
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  handleClick = (event: MouseEvent) => {
+    if (
+      event.target instanceof HTMLElement &&
+      this.node.current &&
+      this.node.current.contains(event.target)
+    )
+      return;
+    this.hide();
+  };
+
+  show = () => this.setState({ isActive: true });
+  hide = () => this.setState({ isActive: false });
 
   render() {
-    const { hoverable, alignment, staticItems, items, children } = this.props;
+    const { hoverable, dropup, alignment, staticItems, items, children } = this.props;
     const { isActive } = this.state;
     return (
       <div
-        className={classNames('dropdown', { 'is-hoverable': hoverable }, alignment, {
-          'is-active': isActive,
-        })}>
+        className={classNames(
+          'dropdown',
+          { 'is-hoverable': hoverable },
+          { 'is-up': dropup },
+          alignment,
+          {
+            'is-active': isActive,
+          }
+        )}>
         <div className="dropdown-trigger">
           {React.Children.map(children, child =>
             React.cloneElement(child as React.ReactElement, {
-              onClick: this.toggleActive,
+              onClick: this.show,
             })
           )}
         </div>
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
             {staticItems}
-            {staticItems && <hr className="dropdown-divider" />}
+            {staticItems && items && <hr className="dropdown-divider" />}
             {items && items.map(dropdown => <DropdownItem key={dropdown.name} {...dropdown} />)}
           </div>
         </div>
