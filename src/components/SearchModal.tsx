a1d3db94b0be2facda04c80eb4ce8ed984e7570f
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-02 15:44:45 -0400

    Restored search modal buttons

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { Search } from './Search';
 
@@ -8,11 +9,25 @@ interface Props {
   hide: () => void;
 }
 
-export const SearchModal: React.FC<Props> = ({ hide }) => (
+export const SearchModal: React.FC<Props> = ({ important, toggleImportant, hide }) => (
   <div className="modal is-active">
-    <div className="modal-background" />
+    <div className="modal-background" onClick={hide} />
     <div className="search-container search-modal modal-content">
-      <Search />
+      <Search>
+        <button
+          className="button is-rounded is-outlined is-light tooltip"
+          data-tooltip={important ? 'Show all' : 'Show only important'}
+          onClick={toggleImportant}>
+          <span className="icon">
+            <i className={classNames('fas', important ? 'fa-list' : 'fa-star')} />
+          </span>
+        </button>
+        <button className="button is-rounded is-outlined is-light" onClick={hide}>
+          <span className="icon">
+            <i className="fas fa-times" />
+          </span>
+        </button>
+      </Search>
     </div>
     <button className="modal-close is-large is-hidden-tablet" onClick={hide} />
   </div>
