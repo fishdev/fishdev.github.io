8d5f3d9fcc3e5afd5a3119deb40af97ef2961c95
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-02 14:12:18 -0400

    Resume update
    Details for The Tartan

--- src/assets/data/blocks/activities/tartan.tsx
+++ src/assets/data/blocks/activities/tartan.tsx
@@ -17,8 +17,26 @@ export const tartan: Block = {
     {
       url: 'https://thetartan.org',
       icon: 'fas fa-newspaper',
-      name: 'Newspaper online edition',
+      name: 'Online edition',
     },
   ],
   description: <p>Writing and editing articles, designing weekly paper layout.</p>,
+  info: (
+    <div>
+      <p>
+        <i>The Tartan</i> is Carnegie Mellon's student newspaper. As the editor of the Science &
+        Technology section, I have drawn upon my experience as an editor and webmaster of my high
+        school student newspaper. Specific responsibilities:
+      </p>
+      <ul>
+        <li>Setting topics and for staff writers every week.</li>
+        <li>Revising articles and coordinating with writers regarding scheduling.</li>
+        <li>
+          Designing 2-3 pages of paper layout using{' '}
+          <a href="https://www.adobe.com/products/indesign.html">Adobe InDesign</a>.
+        </li>
+        <li>Writing 1-2 articles about a STEM-related news event.</li>
+      </ul>
+    </div>
+  ),
 };
