bf772e5bed3ea57c9bd7e7300eb02a8a20ec55ba
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-08-13 10:32:51 -0400

    Added social redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { social } from './assets/data';
 import { Home, Resume, Details, Spotify, NotFound } from './pages';
 
 export const App: React.FunctionComponent = () => {
+  const links = social.map(item => {
+    const name = '/' + item.name!.toLowerCase();
+    const redirect: React.FC = () => {
+      window.location.href = item.url;
+      return null;
+    };
+    return <Route key={name.slice(1)} path={[name, name.slice(0, 2)]} component={redirect} />;
+  });
   return (
     <Router>
       <Switch>
@@ -11,6 +20,7 @@ export const App: React.FunctionComponent = () => {
         <Route path="/resume" exact component={Resume} />
         <Route path="/spotify" exact component={Spotify} />
         <Route path="/404" component={NotFound} />
+        {links}
         <Route path="/:id" component={Details} />
       </Switch>
     </Router>
