2ceb92e2b5122194eabc83e4ebe9b4bb22f59192
Ashwin Srinivasan <ashwin.srinivasan@broadcom.com>
2019-08-13 13:56:04 -0400

    Switched to react-ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
 
 import { social } from './assets/data';
 import { Home, Resume, Details, Spotify, NotFound } from './pages';
 
-export const App: React.FunctionComponent = () => {
-  const links = social.map(item => {
-    const name = '/' + item.name!.toLowerCase();
-    const redirect: React.FC = () => {
-      window.location.href = item.url;
-      return null;
-    };
-    return <Route key={name.slice(1)} path={[name, name.slice(0, 2)]} component={redirect} />;
-  });
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/resume" exact component={Resume} />
-        <Route path="/spotify" exact component={Spotify} />
-        <Route path="/404" component={NotFound} />
-        {links}
-        <Route path="/:id" component={Details} />
-      </Switch>
-    </Router>
-  );
-};
+const history = createHistory();
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
+export class App extends React.PureComponent {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  render() {
+    const links = social.map(item => {
+      const name = '/' + item.name!.toLowerCase();
+      const redirect: React.FC = () => {
+        window.location.href = item.url;
+        return null;
+      };
+      return <Route key={name.slice(1)} path={[name, name.slice(0, 2)]} component={redirect} />;
+    });
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/resume" exact component={Resume} />
+          <Route path="/spotify" exact component={Spotify} />
+          <Route path="/404" component={NotFound} />
+          {links}
+          <Route path="/:id" component={Details} />
+        </Switch>
+      </Router>
+    );
+  }
+}
