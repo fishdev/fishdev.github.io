2ffc5fb154108de8ef25a2eac28db47ae312db61
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-12-27 15:00:37 -0500

    Added future Stripe experience

---
+++ src/assets/data/blocks/experience/stripe.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Block } from '../../../../interfaces';
+
+export const broadcom: Block = {
+  id: 'stripe',
+  name: 'Software Engineering Intern',
+  extra: 'Stripe',
+  range: {
+    start: {
+      month: 5,
+      year: 2020,
+    },
+  },
+};
