907ac14c55d6154860144cac012764f9d6428401
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2020-04-14 20:33:32 -0400

    Fix oval links

--- src/resume/core/components/OvalBlock.tsx
+++ src/resume/core/components/OvalBlock.tsx
@@ -4,11 +4,8 @@ import { Link } from 'react-router-dom';
 import { Block } from '../interfaces';
 
 export const OvalBlock: React.FC<Block> = ({ id, name, extra }) => (
-  <div className="box oval has-text-centered">
-    {/* <BlockHeader hasMore {...block} /> */}
-    <h5 className="title is-4">
-      <Link to={'/' + id}>{name}</Link>
-    </h5>
+  <Link to={'/' + id} className="box oval has-text-centered">
+    <h5 className="title is-4">{name}</h5>
     {extra && <h6 className="subtitle is-6 has-text-grey">{extra}</h6>}
-  </div>
+  </Link>
 );
