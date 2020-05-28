20f1e4cf289bb08761787f1c09116979a43d2468
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2020-05-27 20:50:35 -0400

    Fix type issue

--- src/gallery/util/photoCounts.ts
+++ src/gallery/util/photoCounts.ts
@@ -2,7 +2,7 @@ import { flattenPhotos } from './flattenPhotos';
 import { getData } from '../../data';
 
 export const getGallerySize = () =>
-  flattenPhotos(getData().photos ? getData().photos.gallery : []).length;
+  flattenPhotos(getData().photos ? getData().photos!.gallery : []).length;
 
 export const userHasSeenGallery = () => {
   const store = localStorage.getItem('gallery_count') || '0';
