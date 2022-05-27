bcf4ee937fdf55438c8699382335edefefa5bc62
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-05-27 23:49:16 +0000

    Deploy fishdev/fishdev.github.io to github.com/fishdev/fishdev.github.io.git:master

---
+++ precache-manifest.1ffd9ffbd55fe3faf7609213794d96bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65be1001cd32bb5891253fb65f00904f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9da9972f9bb5c8caa883",
+    "url": "/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "3e06e04e4df48e500ec2",
+    "url": "/static/css/main.a324b66e.chunk.css"
+  },
+  {
+    "revision": "9da9972f9bb5c8caa883",
+    "url": "/static/js/2.225db609.chunk.js"
+  },
+  {
+    "revision": "3df2619e99e84e2c78d68bbb2c93ab7e",
+    "url": "/static/js/2.225db609.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e06e04e4df48e500ec2",
+    "url": "/static/js/main.22c6c109.chunk.js"
+  },
+  {
+    "revision": "d9e44046652d47140091",
+    "url": "/static/js/runtime-main.e3873197.js"
+  },
+  {
+    "revision": "0cd6bd6d41f12e6f71439842423336bf",
+    "url": "/static/media/ashwin.0cd6bd6d.jpg"
+  },
+  {
+    "revision": "b3d2810aa527051349a1229854442138",
+    "url": "/static/media/pgh.b3d2810a.jpg"
+  }
+]);
